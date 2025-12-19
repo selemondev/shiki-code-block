5a76feab7ab7d3c0c1743109da0f6e5913ccee56
selemondev <selemondev19@gmail.com>
2025-12-18 17:40:17 -0700

    chore: remove async keyword from func

--- packages/vue/src/lib/utils/codeToHtml.ts
+++ packages/vue/src/lib/utils/codeToHtml.ts
@@ -1,6 +1,6 @@
 import { type BundledLanguage, codeToHtml, type ShikiTransformer } from "shiki";
 import type { ConvertOptions } from "@/types/theme.interface";
-export async function convertCodeToHtml(
+export function convertCodeToHtml(
 	code: string,
 	lang: BundledLanguage,
 	options: ConvertOptions & {
